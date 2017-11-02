aff6ca5353f9943d8c391429828df63fa1872707
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-02 12:36:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array = array.unshift(element)
+  console.log(array)
   return array
 }
 
