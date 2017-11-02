0a319b99473cd227750fcac042f899205802e669
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-02 12:31:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  return array.push(element)
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
